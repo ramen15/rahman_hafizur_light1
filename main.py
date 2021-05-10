a1c7f7fa19686ddd78d9a89f505ea74454c6d63c
ramen15 <72809089+ramen15@users.noreply.github.com>
2021-05-10 19:01:58 -0400

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-while True:
-    print("Light Level:" + input.light_level())
-    if input.light_level() > 5:
-        light.set_all(light.rgb(0,0,0))
-    else:
-        light.set_all(light.rgb(255,255,255))
-
+soil_reading = 0
+dry_value = 1500
 
 while True:
     print("Light Level: " + input.light_level())
-    if input.light_level() > 15:
-        light.clear()
-    elif input.light_level() >= 6:
-        light.clear()
+    if input.light_level() > 10:
+        light.set_pixel_color(0, light.rgb(0,255,0))
+        light.set_pixel_color(1, light.rgb(0,255,0))
+        light.set_pixel_color(2, light.rgb(0,255,0))
+        light.set_pixel_color(3, light.rgb(0,255,0))
+        light.set_pixel_color(4, light.rgb(0,255,0))
     else:
-        light.set_all(color.rgb(255,165,0))
-#thresholds can change from person to person
+        light.set_pixel_color(0, light.rgb(255,0,0))
+        light.set_pixel_color(1, light.rgb(255,0,0))
+        light.set_pixel_color(2, light.rgb(255,0,0))
+        light.set_pixel_color(3, light.rgb(255,0,0))
+        light.set_pixel_color(4, light.rgb(255,0,0))
+    
+def on_forever():
+    pass
+    soil_reading = input.pinA1.value()
+    print("Soil Reading: " + soil_reading)
+    if (soil_reading <= dry_value):
+        light.set_pixel_color(5, light.rgb(0,0,255))
+        light.set_pixel_color(6, light.rgb(0,0,255))
+        light.set_pixel_color(7, light.rgb(0,0,255))
+        light.set_pixel_color(8, light.rgb(0,0,255))
+        light.set_pixel_color(9, light.rgb(0,0,255))
+    else:
+        light.set_pixel_color(5, light.rgb(0,255,255))
+        light.set_pixel_color(6, light.rgb(0,255,255))
+        light.set_pixel_color(7, light.rgb(0,255,255))
+        light.set_pixel_color(8, light.rgb(0,255,255))
+        light.set_pixel_color(9, light.rgb(0,255,255))
+        pause(2000)
+    forever(on_forever)
+    
--- main.ts
+++ main.ts
@@ -1,20 +1,19 @@
-while (true) {
-    console.log("Light Level:" + input.lightLevel())
-    if (input.lightLevel() > 5) {
-        light.setAll(light.rgb(0, 0, 0))
-    } else {
-        light.setAll(light.rgb(255, 255, 255))
-    }
-    
-}
+let soil_reading = 0
+let dry_value = 1500
 while (true) {
     console.log("Light Level: " + input.lightLevel())
-    if (input.lightLevel() > 15) {
-        light.clear()
-    } else if (input.lightLevel() >= 6) {
-        light.clear()
+    if (input.lightLevel() > 10) {
+        light.setPixelColor(0, light.rgb(0, 255, 0))
+        light.setPixelColor(1, light.rgb(0, 255, 0))
+        light.setPixelColor(2, light.rgb(0, 255, 0))
+        light.setPixelColor(3, light.rgb(0, 255, 0))
+        light.setPixelColor(4, light.rgb(0, 255, 0))
     } else {
-        light.setAll(color.rgb(255, 165, 0))
+        light.setPixelColor(0, light.rgb(255, 0, 0))
+        light.setPixelColor(1, light.rgb(255, 0, 0))
+        light.setPixelColor(2, light.rgb(255, 0, 0))
+        light.setPixelColor(3, light.rgb(255, 0, 0))
+        light.setPixelColor(4, light.rgb(255, 0, 0))
     }
     
 }
